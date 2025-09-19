60004434aa00739a5aedab98429839464defeec2
yuria419 <tjg-690-grm@e-net.nara.jp>
2025-09-19 06:18:44 +0000

    javascriptの追加 ・自機の描画・移動 ・敵の追加

---
+++ game/main.js
@@ -0,0 +1,40 @@
+document.getElementById("shirushi").innerText = "これはゲームです";
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+
+let x = 225;
+let tama = 0;
+window.addEventListener("keydown" , (e) => {
+    if (e.key === "ArrowLeft"){
+        x -= 10;
+    } else if (e.key === "ArrowRight"){
+        x += 10;
+    } else if (e.key === "space") {
+        tama += 1;
+    }
+       
+});
+
+let y1 = 0;
+let y2 = -150;
+
+function gameLoop() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x,480,30,30);
+    ctx.fillStyle = "red";
+    ctx.fillRect(150,y,30,30);
+    y +=1
+    ctx.fillStyle = "red";
+    ctx.fillRect(300,y,30,30);
+    y +=1
+    if (tama > 0) {
+        ctx.fillStyle = "white";
+        ctx.fillRect(x + 10,480 - tama * 10,10,10);
+    }
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
